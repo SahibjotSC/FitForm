c6ac11bc715ff753b34c0d791772268f8b6dd342
SahibjotSC <sahibjotsc@gmail.com>
2024-11-24 18:56:02 -0800

    Added sidebar menu

--- app/src/main/java/com/example/fitform/MainActivity.kt
+++ app/src/main/java/com/example/fitform/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.fitform
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.fitform.databinding.ActivityMainBinding
+import com.example.fitform.fragment.CameraFragment
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var drawerLayout: DrawerLayout? = null
     private lateinit var activityMainBinding: ActivityMainBinding
     private val viewModel : MainViewModel by viewModels()
 
@@ -13,10 +25,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar) //Ignore red line errors
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout?
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.open_nav,
+            R.string.close_nav
+        )
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,
+                    HomeFragment()
+                ).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container1, CameraFragment()) // Default fragment for secondary container
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,
+                    HomeFragment()
+                ).commit()
+
+            R.id.nav_settings -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,
+                    SettingsFragment()
+                ).commit()
+
+            R.id.nav_about -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,
+                    AboutFragment()
+                ).commit()
+
+            R.id.nav_logout -> Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout?.closeDrawer(GravityCompat.START)
+        return true
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
+        if (drawerLayout?.isDrawerOpen(GravityCompat.START) == true) {
+            drawerLayout?.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed()
+            finish()
+        }
     }
 }
